9e9367dd7ee4048fb757843a23ae398cee99fe5d
Mistral Oz <mistral@ozm.fr>
2015-02-20 15:47:38 +0100

    Divers ajout / correctifs : * Maintient les liens en #href dans la barre d'adresse * Autorise l'affichage des popup (reforme) directement depuis le lien #href * Fix un bug du menu lorsqu'on charge un lien #href (avant le scroll) * Change les couleurs des puces pour les étapes réalisées (gris) * Ajout un titre et un descriptif sur la partie "outils" * Modifie la couleur en bleu en sous titre de "contactez nous"

--- js/agency.js
+++ js/agency.js
@@ -11,8 +11,39 @@ $(function() {
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
-        event.preventDefault();
+        //event.preventDefault();
+        //$(location).attr('href', '#toto');
+        event.stopPropagation();
     });
+    // Ajout Mistral : Permets de quitter la modal au click exterieur
+    $('.portfolio-modal').on('click', '.modal-content', function(event){
+    	console.log($(this));
+    	if($(this).hasClass('modal-content')) {
+	    	$(this).closest('.portfolio-modal').modal('hide');
+    	}
+    });
+    // Ajout Mistral : Evite le comportement ci-dessus lors d'un click interne
+    $('.portfolio-modal').on('click', '.modal-body', function(event){
+	    event.stopPropagation();
+	});
+	// Ajout Mistral : Patch les modals pour qu'elles affichent bien le href # dans l'url
+    $('#Reforme').on('click', '.portfolio-link', function(event){
+    	$($(this).attr('href')).modal('show');
+	    event.stopPropagation();
+	});
+	// Ajout Mistral : Test si l'url contient un href qui correspond à une modal : auquel cas, l'affiche
+	var hreftag = $(location).attr('href').replace(/^(.*)#(.*)/i, '$2');
+    $('.portfolio-modal[id="'+hreftag+'"]').modal('show');
+	$('#Reforme .portfolio-link[href="#Reforme-DRM"]');
+    // Ajout Mistral : Patch la navbar qui n'applique pas le bon style au chargement
+	var patchScrollNavbar = function() {
+		var y = window.pageYOffset || docElem.scrollTop;
+		if ( y >= 300 ) {
+			classie.add( document.querySelector( '.navbar-default' ), 'navbar-shrink' );
+		}
+	};
+	patchScrollNavbar();
+	//setTimeout( cbpAnimatedHeader.scrollPage, 2250 );
 });
 
 // Highlight the top nav as scrolling occurs
